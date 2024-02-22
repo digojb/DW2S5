f03df41b48f60d293d0a870b131adf368d5358a6
Diogo José Bernardo <103767375+digojb@users.noreply.github.com>
2024-02-22 10:44:57 -0300

    first

---
+++ AulaPHP/segunda.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1><?php echo "Essa é minha segunda página PHP" ?></h1>
+<p><?php echo "Essa é uma outra pagina" ?> </p>
+
+<a href="/hello.php"><?php echo "voltar" ?> </a>
+
+</body>
+
+</html>
