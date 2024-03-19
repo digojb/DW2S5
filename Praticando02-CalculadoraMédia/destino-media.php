d92f90d92f55adc4057837669ec4a0b8d3adb752
Diogo José Bernardo <103767375+digojb@users.noreply.github.com>
2024-03-19 19:32:42 -0300

    Update destino-media.php

--- Praticando02-CalculadoraMédia/destino-media.php
+++ Praticando02-CalculadoraMédia/destino-media.php
@@ -21,7 +21,7 @@ if ($media < 4) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calculadora média</title>
 </head>
 
 <body>
