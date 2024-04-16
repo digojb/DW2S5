8ceda769c4a949d7928316edfcc6914717903195
Diogo José Bernardo <103767375+digojb@users.noreply.github.com>
2024-04-16 08:26:14 -0300

    first

---
+++ aula-08/ex01/sobre.php
@@ -0,0 +1,17 @@
+<?php require_once 'cabecalho.php'; ?>
+
+<div class="inicio">
+    <div class="bg-light p-4 mb-4 rounded">
+        <h1 class="text-center">Sobre</h1>
+    </div>
+
+    <img class="img-lorem img-thumbnail m-4 rounded float-end" src="https://2.bp.blogspot.com/-fD6BjedxEgU/VBSCM1T_WdI/AAAAAAAADiQ/eZxEUtjiY7s/s1600/ara%C3%A7ari-mulato%2B-%2BOpenBrasil.org.png" alt="Lorem Ipsum">
+    <p>
+        O araçari-mulato é uma ave Piciforme da família Ramphastidae. É considerado por alguns especialistas, o mais bonito representante da família dos tucanos e araçaris.
+    </p>
+    <p>
+        Presente na Amazônia brasileira ao sul dos rios Solimões e Madeira (Amazonas) e nos altos cursos dos rios Tapajós e Xingu (Mato Grosso). Encontrado também na Bolívia, Peru e Colômbia, na fronteira com o Brasil.
+    </p>
+</div>
+
+<?php require_once 'rodape.php'; ?>
